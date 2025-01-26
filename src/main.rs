e06c42205aa1533f275e383d506fae581a32f180
Shun E <shun-eto@sopherre.com>
2025-01-26 06:42:29 +0000

    起動コマンドの変更

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, World!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde_json::json;
+
+#[actix_web::main]
+async fn main() -> Result<(), actix_web::Error> {
+    let _ = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+            .route("/echo", web::post().to(echo))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await;
+
+    Ok(())
+}
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!!!")
+}
+
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().json(json!(req_body))
 }
